1e28ff4e7aba6da05e24d26d8f4bb8edcea44695
anyrun63 <telsonltd@gmail.com>
2024-09-07 22:21:00 +0700

    Создано окно и виджеты

--- Cats.py
+++ Cats.py
@@ -1,1 +1,19 @@
 from tkinter import *
+from PIL import Image, ImageTk
+import requests
+from io import BytesIO
+
+window = Tk()
+window.title("Cats!")
+window.geometry("600x480")
+
+label = Label()
+label.pack()
+
+url = "https://dog.ceo/api/breeds/image/random"
+img = load_image(url)
+
+if img:
+    label.config(image=img)
+    label.image = img #чтобы мусорщик не убрал
+    window.mainloop()
