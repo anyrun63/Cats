6e8286509b3700aabcc8a89eab6edfd906e7644d
anyrun63 <telsonltd@gmail.com>
2024-09-07 22:47:22 +0700

    Добавили кнопку для загрузки нового изображения

--- Cats.py
+++ Cats.py
@@ -6,7 +6,7 @@ from io import BytesIO
 from bottle import response
 
 
-def load_image():
+def load_image(url):
     try:
         response = requests.get(url)
         response.raise_for_status() #для обработки исключений
@@ -17,6 +17,12 @@ def load_image():
         print(f"Произошла ошибка: {e}")
         return None
 
+def set_image():
+    img = load_image(url)
+    if img:
+        label.config(image=img)
+        label.image = img  # чтобы мусорщик не убрал
+
 window = Tk()
 window.title("Cats!")
 window.geometry("600x480")
@@ -24,10 +30,11 @@ window.geometry("600x480")
 label = Label()
 label.pack()
 
+update_button = Button(text="Обновить", command=set_image)
+update_button.pack()
+
 url = "https://dog.ceo/api/breeds/image/random"
 img = load_image(url)
+set_image()
 
-if img:
-    label.config(image=img)
-    label.image = img #чтобы мусорщик не убрал
-    window.mainloop()
+window.mainloop()
